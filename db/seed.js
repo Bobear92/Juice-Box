121945bd76edd92b86aa9f5c66325701b785ba8c
Robert <robwoolbright@gmail.com>
2021-10-16 09:13:41 -0400

    database up and running

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,4 @@
+const { CommandCompleteMessage } = require("pg-protocol/dist/messages");
 const {
   client,
   getAllUsers,
@@ -6,8 +7,8 @@ const {
   createPost,
   updatePost,
   getAllPosts,
-  getPostsByUser,
   getUserById,
+  getPostsByTagName,
 } = require("./index");
 
 async function createInitialUsers() {
@@ -48,16 +49,33 @@ async function createInitialUsers() {
 async function createInitialPosts() {
   try {
     const [albert, sandra, glamgal] = await getAllUsers();
+    console.log("Starting to create posts...");
 
     await createPost({
       authorId: albert.id,
       title: "First Post",
       content:
         "This is my first post. I hope I love writing blogs as much as I love writing them.",
+      tags: ["#happy", "#youcandoanything"],
     });
+    await createPost({
+      authorId: sandra.id,
+      title: "How does this work?",
+      content: "Seriously, does this even do anything?",
+      tags: ["#happy", "#worst-day-ever"],
+    });
+
+    await createPost({
+      authorId: glamgal.id,
+      title: "Living the Glam Life",
+      content: "Do you even? I swear that half of you are posing.",
+      tags: ["#happy", "#youcandoanything", "#canmandoeverything"],
+    });
+    console.log("Finished creating posts!");
 
     // a couple more
   } catch (error) {
+    console.error("Error creating posts!");
     throw error;
   }
 }
@@ -67,6 +85,8 @@ async function dropTables() {
     console.log("Starting to drop tables...");
 
     await client.query(`
+        DROP TABLE IF EXISTS post_tags;
+        DROP TABLE IF EXISTS tags;
         DROP TABLE IF EXISTS posts;
         DROP TABLE IF EXISTS users;
       `);
@@ -102,6 +122,19 @@ async function createTables() {
         active BOOLEAN DEFAULT true
       );
       `);
+    await client.query(`
+      CREATE TABLE tags (
+        id SERIAL PRIMARY KEY, 
+        name varchar(255) UNIQUE NOT NULL
+      );
+      `);
+    await client.query(`
+      CREATE TABLE post_tags (
+        "postId" INTEGER REFERENCES posts(id), 
+        "tagId" INTEGER REFERENCES tags(id), 
+        UNIQUE ("postId", "tagId")
+      );
+      `);
 
     console.log("Finished building tables!");
   } catch (error) {
@@ -119,6 +152,7 @@ async function rebuildDB() {
     await createInitialUsers();
     await createInitialPosts();
   } catch (error) {
+    console.error("Error duing rebuildDB");
     throw error;
   }
 }
@@ -153,6 +187,16 @@ async function testDB() {
     console.log("Calling getUserById with 1");
     const albert = await getUserById(1);
     console.log("Result:", albert);
+
+    console.log("Calling updatePost on posts[1], only updating tags");
+    const updatePostTagsResult = await updatePost(posts[1].id, {
+      tags: ["#youcandoanything", "#redfish", "#bluefish"],
+    });
+    console.log("Result:", updatePostTagsResult);
+
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
 
     console.log("Finished database tests!");
   } catch (error) {
