650928f8cee2084e39b45a68232665e20b26a90f
Robert <robwoolbright@gmail.com>
2021-10-17 19:24:59 -0400

    finished part 3, belive everything is working

--- api/tags.js
+++ api/tags.js
@@ -1,17 +1,50 @@
 const express = require("express");
+const { getAllTags, getPostsByTagName } = require("../db");
 const tagsRouter = express.Router();
 
-const { getAllTags } = require("../db");
+tagsRouter.get("/", async (req, res) => {
+  try {
+    const tags = await getAllTags();
 
-tagsRouter.use((req, res, next) => {
-  console.log("A request is being made to /tags");
-  next();
+    res.send({
+      tags,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-tagsRouter.get("/", async (req, res, next) => {
-  const tags = await getAllTags();
-  res.send({
-    tags,
-  });
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
+
+  try {
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+    const getPosts = await getPostsByTagName(tagName);
+
+    const posts = getPost.filter((post) => {
+      // the post is active, doesn't matter who it belongs to
+      if (post.active) {
+        return true;
+      }
+
+      // the post is not active, but it belogs to the current user
+      if (req.user && post.author.id === req.user.id) {
+        return true;
+      }
+
+      // none of the above are true
+      return false;
+    });
+
+    res.send({
+      posts,
+    });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
 });
+
 module.exports = tagsRouter;
